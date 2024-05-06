597f84465133f4f7658fa1b04cad97c9af7a97b5
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-05-05 18:51:44 -0700

    implement Sign-In Form/Route with reusable CustomInput component with Zod ShadCn for Form Submission

---
+++ components/CustomInput.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import React from 'react'
+import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Control, FieldPath } from 'react-hook-form/dist'
+import { z } from 'zod'
+import { authFormSchema } from '@/lib/utils'
+
+interface CustomInput {
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: FieldPath<z.infer<typeof authFormSchema>>,
+    label: string,
+    placeholder: string,
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }: any ) => (
+                <div className='form-item'>
+                    <FormLabel className='form-label'>
+                        {label}
+                    </FormLabel>
+                    <div className='flex w-full flex-col'>
+                        <FormControl>
+                            <Input 
+                                placeholder={placeholder}
+                                className='input-class'
+                                // Spread Field Properties for React HookForm to Work!
+                                {...field}
+                                type={ name === 'password' ? 'password' : 'text' }
+                            />
+                        </FormControl>
+                        <FormMessage 
+                            className='form-message mt-2'
+                        />
+                    </div>
+                </div>
+            )}
+        />
+    )
+}
+
+export default CustomInput

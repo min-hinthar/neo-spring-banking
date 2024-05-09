30645ecfc0d39958069d2d44b8733fbba38caf7b
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-05-09 16:46:30 -0700

    implement Category feature to display Transaction Categories using ShadCn Progress UI, ready for production by ignoreBuildErrors in next.config.mjs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -50,7 +50,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
       <RightSidebar
         user={loggedIn}
-        transactions={accounts?.Transactions}
+        transactions={account?.transactions}
         banks={accountsData?.slice(0, 2)}
       />
     </section>
